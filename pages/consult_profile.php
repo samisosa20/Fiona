4db41bfa3e6b957cd1e3b444a512e98a8c3484f4
Sammy <1449@holbertonschool.com>
2020-03-21 20:46:18 +0000

    search info profile

---
+++ pages/consult_profile.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+$id_user = $_SESSION["Id_user"];
+include_once("../conexions/connect.php");
+$action=$_GET["action"];
+$insert = "SELECT * FROM fionadb.users WHERE id_user='$id_user'";
+$ejecutar =mysqli_query( $conn,$insert);
+$lista = mysqli_fetch_array($ejecutar);
+$name = $lista["name"];
+$last_name = $lista["last_name"];
+$email = $lista["email"];
+if ($action == 1) {
+    echo $name;
+} else if ($action == 2){
+    echo $last_name;
+} else if ($action == 3){
+    echo $email;
+}
+mysqli_close($conn);
+?>
