88918619e2b19b7665fa0d6c0ab558c10f23d8d9
Sammy <1449@holbertonschool.com>
2020-03-23 23:15:02 +0000

    change consult to select catego

--- pages/consult_cate.php
+++ pages/consult_cate.php
@@ -4,11 +4,20 @@ $id_user = $_SESSION["Id_user"];
 include_once("../conexions/connect.php");
 $insert = "SELECT * FROM fionadb.categorias WHERE id_user='$id_user'";
 $ejecutar =mysqli_query( $conn,$insert);
-echo "<option value='0' selected>Selecciona una opción</option>";
+if (!$_GET["act"]){
+    echo "<option value='0' selected>Selecciona una opción</option>";
+    $select = 0;
+} else{
+    $select = $_GET["act"];
+}
 while ($lista = mysqli_fetch_array($ejecutar)){
     $id = $lista["id"];
     $nombre = $lista ["categoria"];
-    echo "<option value='$id'>$nombre</option>";
+    if ($select == $id){
+        echo "<option value='$id' selected>$nombre</option>";
+    } else {
+        echo "<option value='$id'>$nombre</option>";
+    }
 }
 mysqli_close($conn);
 ?>
