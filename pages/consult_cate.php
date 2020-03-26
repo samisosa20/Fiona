1b4053a3aa2263698b59e1f02fb89573c0d713bb
Sammy <1449@holbertonschool.com>
2020-03-26 05:23:04 +0000

    fix value 0

--- pages/consult_cate.php
+++ pages/consult_cate.php
@@ -2,14 +2,14 @@
 session_start();
 $id_user = $_SESSION["Id_user"];
 include_once("../conexions/connect.php");
-$insert = "SELECT * FROM fionadb.categorias WHERE id_user='$id_user'";
+$insert = "SELECT * FROM fionadb.categorias WHERE id_user='$id_user' and grupo != 5";
 $ejecutar =mysqli_query( $conn,$insert);
 if (!$_GET["act"]){
-    echo "<option value='0' selected>Selecciona una opción</option>";
     $select = 0;
 } else{
     $select = $_GET["act"];
 }
+echo "<option value='0' selected>Sin categoria</option>";
 while ($lista = mysqli_fetch_array($ejecutar)){
     $id = $lista["id"];
     $nombre = $lista ["categoria"];
