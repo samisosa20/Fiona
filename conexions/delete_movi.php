268316da0361b674abf92c22917a585c732918e3
Sammy <1449@holbertonschool.com>
2020-03-27 16:48:39 +0000

    add parameters to delete transfer

--- conexions/delete_movi.php
+++ conexions/delete_movi.php
@@ -3,8 +3,9 @@
     $id_user = $_SESSION["Id_user"];
     include_once("connect.php");
     $id = $_POST["id"];
+    $fecha = $_POST["fecha"];
     $delete = "DELETE FROM fionadb.movimientos
-    WHERE id=$id and id_user='$id_user';";
+    WHERE (id=$id or id_transfe=$id) and fecha='$fecha' and id_user='$id_user';";
     $save = mysqli_query($conn, $delete);
     if(!$save){ 
         echo 400;
